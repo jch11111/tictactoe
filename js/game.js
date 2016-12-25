bfc461bce60ad93944d859a8307af7d8e4561022
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2016-12-25 14:24:07 -0800

    displaying the basic game grid - responsive

---
+++ js/game.js
@@ -0,0 +1,39 @@
+var game = (function () {
+    var VERTICAL = 0,
+        HORIZONTAL = 1;
+
+    function drawLine(fromX, fromY, length, verticalOrHorizontal, width) {
+        var x2, x3, x4, y2, y3, y4;
+
+        if (VERTICAL === verticalOrHorizontal) {
+            x2 = fromX;
+            x3 = x4 = fromX - width;
+            y2 = y3 = fromY + length;
+            y4 = fromY;
+        } else {
+            y2 = fromY;
+            y3 = y4 = fromY - width;
+            x2 = x3 = fromX + length;
+            x4 = fromX;
+        }
+
+        new Path()
+            .moveTo(fromX, fromY)
+            .lineTo(x2, y2)
+            .lineTo(x3, y3)
+            .lineTo(x4, y4)
+            .closePath()
+            .fill('blue')
+            .addTo(stage);
+    }
+    return {
+        drawLine: drawLine
+    }
+})();
+
+
+game.drawLine(75, 0, 225, 0, 2);
+game.drawLine(150, 0, 225, 0, 2);
+game.drawLine(0, 75, 225, 1, 2);
+game.drawLine(0, 150, 225, 1, 2);
+
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,38 +1,51 @@
 var tictactoe = (function () {
 
+    var VERTICAL = 0,
+        HORIZONTAL = 1;
+
     function init() {
         $(function () {
             setEventHandlers();
 
-            var canvas = oCanvas.create({
-                canvas: "#respondCanvas"
+/*            
+            bonsai.run(document.getElementById('game'), {
+
+                code: function () {
+
+                    new Path()
+                        .moveTo(50, 0)
+                        .lineTo(50, 200)
+                        .lineTo(52, 200)
+                        .lineTo(52, 0)
+                        .closePath()
+                        .fill('blue')
+                        .addTo(stage);
+
+                    new Path()
+                        .moveTo(150, 0)
+                        .lineTo(150, 200)
+                        .lineTo(152, 200)
+                        .lineTo(152, 0)
+                        .closePath()
+                        .fill('blue')
+                        .addTo(stage);
+
+                },
+
+                width: 200,
+                height: 300
+            });
+  */
+
+            bonsai.run(document.getElementById('game'), {
+                url: 'js/game.js',
+                width: 225,
+                height: 225
             });
 
-            var line = canvas.display.line({
-                start: { x: 5, y: 15 },
-                end: { x: 10, y: 25 },
-                stroke: "2px #0aa",
-                cap: "round"
-            });
 
-            canvas.addChild(line);
 
-            var c = $('#respondCanvas');
-            var ct = c.get(0).getContext('2d');
-            var container = $(c).parent();
 
-            //Run function when browser resizes
-            $(window).resize( respondCanvas );
-
-            function respondCanvas(){
-                c.attr('width', $(container).width() ); //max width
-                c.attr('height', $(container).height() ); //max height
-
-                //Call a function to redraw other content (texts, images etc)
-            }
-
-            //Initial call
-            //respondCanvas();
 
             drawGame();
         })
