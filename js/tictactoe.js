d1dcc2a7129d15ca666f66bcfaf53a93eabdcd32
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2016-12-30 13:44:21 -0800

    moved gameGrid to a separate file

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -8,67 +8,6 @@ var tictactoe = (function () {
         O = 'O',
         rowAndColumnHeight = 75,
         nonClickableDivider = 5,
-        gameGrid = {
-            squares: [
-                {squareNumber: 0, origin: { x: 0, y: 0 }, position: {row: 0, col: 0, diag1: true } },
-                {squareNumber: 1,  origin: { x: rowAndColumnHeight, y: 0 }, position: {row: 0, col: 1} },
-                {squareNumber: 2,  origin: { x: rowAndColumnHeight * 2, y: 0 }, position: {row: 0, col: 2, diag2: true } },
-                {squareNumber: 3,  origin: { x: 0, y: rowAndColumnHeight }, position: {row: 1, col: 0 } },
-                {squareNumber: 4,  origin: { x: rowAndColumnHeight, y: rowAndColumnHeight }, position: {row: 1, col: 1, diag1: true, diag2: true } },
-                {squareNumber: 5,  origin: { x: rowAndColumnHeight * 2, y: rowAndColumnHeight }, position: {row: 1, col: 2 } },
-                {squareNumber: 6,  origin: { x: 0, y: rowAndColumnHeight * 2 }, position: {row: 2, col: 0, diag2: true } },
-                {squareNumber: 7,  origin: { x: rowAndColumnHeight, y: rowAndColumnHeight * 2 }, position: {row: 2, col: 1 } },
-                {squareNumber: 8,  origin: { x: rowAndColumnHeight * 2, y: rowAndColumnHeight * 2 }, position: {row: 2, col: 2, diag1: true } }
-            ],
-            getStripe: function (direction, rowOrColumnNumber) {
-                return this.squares.filter(function (square) {
-                    if ('diag1' === direction || 'diag2' === direction) {
-                        return square.position[direction];
-                    } else {
-                        return square.position[direction] === rowOrColumnNumber;
-                    }
-                })
-            },
-
-            getXsAndOs: function (stripe) {
-                return stripe.map(function (square) {
-                    return square.xOrO;
-                })
-                .join('');
-            },
-            getStripesMeetingCriteria: function (criteriaFunction) {
-                var stripes = [],
-                    me = this;
-
-                getStripes('row');
-                getStripes('col');
-                getStripes('diag1');
-                getStripes('diag2');
-
-                function getStripes(direction) {
-                    var stripe;
-                    if ('diag1' === direction || 'diag2' === direction) {
-                        stripe = me.getStripe(direction);
-                        if (criteriaFunction(stripe)) {
-                            stripes.push(stripe);
-                        }
-                    } else {
-                        for (var squareNumber = 0; squareNumber <= 2; squareNumber++) {
-                            stripe = me.getStripe(direction, squareNumber);
-                            if (criteriaFunction(stripe)) {
-                                stripes.push(stripe);
-                            }
-                        }
-                    }
-                }
-                return stripes;
-            },
-            getNumberOfOccupiedSquares: function() {
-                return this.squares.filter(function (square) {
-                    return square.xOrO;
-                }).length;
-            }
-        },
         padding = 0.2 * rowAndColumnHeight,
         whoseTurn = USER,
         XXX = 'XXX',
